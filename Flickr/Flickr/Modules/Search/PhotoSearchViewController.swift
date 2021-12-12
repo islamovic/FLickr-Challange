f357b3c65976e8d173737ed074db084215d33c5b
islamovic <islam.abo.elsoud@gmail.com>
2021-12-12 17:40:05 +0200

    land on searcn photos scene

---
+++ Flickr/Flickr/Modules/Search/PhotoSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoSearchViewController.swift
+//  Flickr
+//
+//  Created by Islam Ibrahim on 12/12/2021.
+//
+
+import UIKit
+
+class PhotoSearchViewController: UIViewController {
+
+    @IBOutlet private weak var imageSearchBar: UISearchBar!
+    @IBOutlet private weak var searchResultCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
